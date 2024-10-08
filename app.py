831cb3b7ada89a2623ef8446fab0cf292bcb7027
baasilali <108046643+baasilali@users.noreply.github.com>
2024-10-07 22:51:49 -0700

    Add files via upload
    "Live Server" by  was used to test .HTML
    https://openweathermap.org/ was used for Weather API Key

---
+++ app.py
@@ -0,0 +1,20 @@
+import requests
+
+#API Used From: https://openweathermap.org/api
+
+api_key = 'af58b2902317c0e2ab96a0897bc07e09'
+
+user_input = input('Enter City: ')
+print(user_input)
+
+weather_data = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+
+if weather_data.json()['cod'] == '404':
+    print("No City Found")
+else:
+    weather = weather_data.json()['weather'][0]['main']
+    temp = round(weather_data.json()['main']['temp'])
+
+    print(f"The weather in {user_input} is: {weather}")
+    print(f"The temperature in {user_input} is: {temp}ºF")
